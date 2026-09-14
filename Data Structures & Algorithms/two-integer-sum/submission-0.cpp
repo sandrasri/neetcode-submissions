f4e122a219fdb3e407609c0dadf3357c234c1bdf
Sandra S. <156359768+sandrasri@users.noreply.github.com>
2026-09-14 14:00:47 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,20 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> hashmap;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if (hashmap.find(complement) != hashmap.end()) {
+                return {hashmap[complement], i};
+            }
+
+            hashmap[nums[i]] = i;
+
+        }
+        return {};
+    }
+};
