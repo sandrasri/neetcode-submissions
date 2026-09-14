c9425c2dfea0966cfb220f13f178de7198cd70d8
Sandra S. <156359768+sandrasri@users.noreply.github.com>
2026-09-14 14:40:29 -0400

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        std::unordered_map<char, int> countS;
+        std::unordered_map<char, int> countT;
+
+        for (size_t i = 0; i < s.length(); i++) {
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+
+        return countS == countT;
+    }
+};
